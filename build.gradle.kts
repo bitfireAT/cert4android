3dfcec4e11e1d6ff64b7b8acf00983ce843a1d86
Arnau Mora Gras <arnyminerz@proton.me>
2023-05-23 17:32:20 +0200

    Migrated to Kotlin DSL
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

---
+++ build.gradle.kts
@@ -0,0 +1,9 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.library") version "8.0.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+    id("org.jetbrains.dokka") version "1.8.10" apply false
+}
+
+group = "at.bitfire"
+version = project.properties["cert4android.version"]
