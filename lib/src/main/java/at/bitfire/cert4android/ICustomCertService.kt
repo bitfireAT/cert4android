931fb125784de899f878b58349f1ab86d7d4bac2
Arnau Mora <arnyminerz@proton.me>
2023-08-28 11:31:22 +0200

    Documentation
    Signed-off-by: Arnau Mora <arnyminerz@proton.me>

--- lib/src/main/java/at/bitfire/cert4android/ICustomCertService.kt
+++ lib/src/main/java/at/bitfire/cert4android/ICustomCertService.kt
@@ -1,6 +1,6 @@
 package at.bitfire.cert4android
 
-import kotlinx.coroutines.CompletableDeferred
+import java.security.cert.X509Certificate
 
 interface ICustomCertService {
     @Deprecated(
@@ -9,7 +9,41 @@ interface ICustomCertService {
     )
     fun checkTrusted(rawCert: ByteArray, interactive: Boolean, foreground: Boolean, callback: IOnCertificateDecision)
 
-    fun checkTrusted(rawCert: ByteArray, interactive: Boolean, foreground: Boolean): CompletableDeferred<Boolean>
+    /**
+     * Checks whether the user trusts the given certificate.
+     *
+     * This function can take a long time to complete, if [interactive] is `true`, for example, and the user takes a
+     * while to confirm.
+     * This is why it's recommended to add a timeout.
+     *
+     * Example:
+     * ```kotlin
+     * try {
+     *     withTimeout(30000) {
+     *         val isTrusted = svc.checkTrusted(cert, interactive, appInForeground)
+     *
+     *         if (!isTrusted) {
+     *             throw CertificateNotTrustedException(cert)
+     *         }
+     *     }
+     * } catch (_: TimeoutCancellationException) {
+     *     cert.let(svc::abortCheck)
+     *     throw CertificateTimeoutException()
+     * }
+     * ```
+     *
+     * @since 2023-08-28
+     *
+     * @param cert The certificate to check for trust-worthiness.
+     * @param interactive Whether the user can interact with the decision.
+     * This means that if the certificate is not trusted, a notification will pop up asking the user for confirmation.
+     * If the certificate is not trusted, and [interactive] is `false`, this function will return false.
+     * @param foreground Whether this function is running in the foreground.
+     * If true, the notification will still be shown, but the confirmation activity will be launched automatically.
+     *
+     * @return `true` if the certificate is trusted. `false` otherwise.
+     */
+    suspend fun checkTrusted(cert: X509Certificate, interactive: Boolean, foreground: Boolean): Boolean
 
     @Deprecated(
         message = "Callbacks no longer used",
@@ -17,5 +51,8 @@ interface ICustomCertService {
     )
     fun abortCheck(callback: IOnCertificateDecision)
 
-    fun abortCheck(completable: CompletableDeferred<Boolean>)
+    /**
+     * Abort all checks being made to the given certificate.
+     */
+    fun abortCheck(certificate: X509Certificate)
 }
