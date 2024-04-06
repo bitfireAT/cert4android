bc26caef1c03543008236ecdf13673b32257d349
Ricki Hirner <hirner@bitfire.at>
2024-04-06 20:50:35 +0200

    Remove unnecessary dependency

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -92,7 +92,6 @@ dependencies {
 
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.lifecycle.livedata)
-    implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel)
     implementation(libs.conscrypt)
 
