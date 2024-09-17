7401ae5479289ce4c90a32948671bd3a6314c2fb
Arnau Mora <arnyminerz@proton.me>
2024-09-17 17:09:09 +0200

    Migrated to M3
    Signed-off-by: Arnau Mora Gras <arnyminerz@proton.me>

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -95,7 +95,7 @@ dependencies {
     // Jetpack Compose
     implementation(libs.androidx.activityCompose)
     implementation(platform(libs.compose.bom))
-    implementation(libs.compose.material)
+    implementation(libs.compose.material3)
     implementation(libs.compose.runtime.livedata)
     debugImplementation(libs.compose.ui.tooling)
     implementation(libs.compose.ui.toolingPreview)
