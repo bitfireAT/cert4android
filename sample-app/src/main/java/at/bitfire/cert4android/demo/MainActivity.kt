6d4263c8ac85570f4c1590086aae9c2046034380
Sunik Kupfer <kupfer@bitfire.at>
2025-03-11 16:15:05 +0100

    Make PendingDecision a data class

--- sample-app/src/main/java/at/bitfire/cert4android/demo/MainActivity.kt
+++ sample-app/src/main/java/at/bitfire/cert4android/demo/MainActivity.kt
@@ -138,7 +138,7 @@ class MainActivity : ComponentActivity() {
 
         val resultMessage = MutableLiveData<String>()
 
-        class PendingDecision(
+        data class PendingDecision(
             val certificateDetails: CertificateDetails,
             val userDecision: CompletableDeferred<Boolean>
         )
