3de7b87a2955cad14a2831d69d2ad1b86f792414
Ricki Hirner <hirner@bitfire.at>
2025-04-08 14:46:56 +0200

    Main activity: edge-to-edge

--- sample-app/src/main/java/at/bitfire/cert4android/demo/MainActivity.kt
+++ sample-app/src/main/java/at/bitfire/cert4android/demo/MainActivity.kt
@@ -7,11 +7,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
@@ -52,11 +54,16 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        enableEdgeToEdge()
+
         setContent {
             val snackBarHostState = remember { SnackbarHostState() }
             val pendingDecision = model.pendingDecisionFlow.collectAsStateWithLifecycle().value
 
-            Box(Modifier.fillMaxSize()) {
+            Box(Modifier
+                .fillMaxSize()
+                .safeDrawingPadding()) {
                 Column(
                     Modifier
                         .padding(8.dp)
